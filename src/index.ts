54e53d42f66bf3221263822649abf20a3ab4a755
paradigmzero <josephrcalton@gmail.com>
2023-03-09 18:19:45 +0000

    Pool resulting in too many connections, try Client next

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, {Express, Request, Response} from "express";
 const cors = require('cors');
 const bodyParser = require('body-parser')
-const port : number = 3000;
+const port : number = 7012;
 const app : Express = express();
 require('dotenv').config();
 
@@ -15,6 +15,7 @@ const pool = new Pool({
     password: process.env.PASSWORD,
     port: process.env.PORT
   });
+
 
 // Middleware
 // allow requests from same origin ( e.g. localhost)
@@ -40,11 +41,45 @@ app.get("/:id", async (req: Request, res: Response)=>{
     children.id = childrensHomework.childId
     WHERE homeworkId=$1 ORDER BY id;`,[req.params.id]);
 
-    res.send(aux);
+    res.send(aux.rows);
+});
+
+// Add a single piece of homework, and return the added homework
+app.post("/", async (req: Request, res: Response)=>{
+
+    // req.body is that right?
+    let homework = await pool.query(`INSERT INTO homework (Name, Image, Datedue, Comment) VALUES ($1,$2,$3,$4) RETURNING *;`,
+    [req.body.name,req.body.image, req.body.dateDue, req.body.comment]);
+    let children = await pool.query(`SELECT * FROM children;`);
+
+    console.log(homework);
+    console.log(children);
+
+    for(let i=0; i < children.length; i++)
+    {
+        await pool.query(`INSERT INTO childrensHomework (homeworkid, childid) VALUES ($1,$2);`,[homework.id, children[i].id]);
+    }
+
+
+    res.send(homework);
+});
+
+
+// Update a child's homework, and return the updated value
+app.put("/:homeworkId/:childId", async (req: Request, res: Response)=>{
+    let child = await pool.query(`UPDATE childrensHomework 
+    SET image = $1, comment = $2, annotation = $3 
+    WHERE homeworkid= $4 AND childid = $5 RETURNING *;`,
+    [req.body.image,req.body.comment,req.body.comment,req.params.homeworkId,req.params.childId]);
+
+    res.send(child);
+});
+
+app.get("/test", async (req: Request, res: Response)=>{
+    res.send([1,2,3])
 });
 
 // Delete single homework, and childrens homework
-
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
-})
+});

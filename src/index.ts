650c2704dce3864fa1633521528e8a69fb7f1286
Joseph Alton <josephrcalton@gmail.com>
2024-02-13 14:01:19 +0000

    Add nodemon in dev dependency and setup more template for Swagger openapi docs

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,39 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-const port : number = 7012;
-const app : Express = express();
+const port: number = 7012;
+const app: Express = express();
 import "dotenv-defaults/config";
 import swaggerUI from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
 
-
-
 // used for settings for Swagger
 const options = {
-    failOnErrors: true,
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Webpage word counter API",
-        version: process.env.npm_package_version as string,
-      },
+  failOnErrors: true,
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "HomeWorked backend",
+      version: process.env.npm_package_version as string,
     },
-    apis: ['./src/index.ts']
-  };
-  
+  },
+  apis: ["./src/index.ts"],
+};
+
 const openapiSpecification = swaggerJsDoc(options);
-
-
 
 // REDO  as client
 
 // PostgreSQL database:
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 // Middleware
 // allow requests from same origin ( e.g. localhost)
@@ -52,20 +48,35 @@ app.use("/api", swaggerUI.serve, swaggerUI.setup(openapiSpecification));
  * /:
  *   get:
  *     description: Get all homework
- *     summary: Get all homework
  *     responses:
  *       200:
  *         description: Success - got all homework
- *
  */
-app.get("/", async (req: Request, res: Response)=>{
-    var aux = await pool.query('SELECT * FROM Homework;');
-    res.send(aux.rows);
+app.get("/", async (req: Request, res: Response) => {
+  var aux = await pool.query("SELECT * FROM Homework;");
+  res.send(aux.rows);
 });
 
-// Get single homework with corresponding children
-app.get("/:id", async (req: Request, res: Response)=>{
-    let aux = await pool.query(`
+/**
+ * @openapi
+ * /{id}:
+ *     get:
+ *       description: Get all the children's homework for a given piece of homework
+ *       parameters:
+ *       - in: path
+ *         description: id for the piece of homework
+ *         name: id
+ *         schema:
+ *          type: integer
+ *         required: true
+ *       responses:
+ *          200:
+ *              description: Success - got all homework
+ */
+app.get("/:id", async (req: Request, res: Response) => {
+  console.log(req.params.id);
+  let aux = await pool.query(
+    `
     SELECT children.id, children.name, children.avatar, childrensHomework.image,
     childrensHomework.comment, childrensHomework.annotation
     FROM children
@@ -73,56 +84,95 @@ app.get("/:id", async (req: Request, res: Response)=>{
     childrensHomework
     ON
     children.id = childrensHomework.childId
-    WHERE homeworkId=$1 ORDER BY id;`,[req.params.id]);
+    WHERE homeworkId=$1 ORDER BY id;`,
+    [req.params.id]
+  );
 
-    res.send(aux.rows);
+  console.log(aux);
+
+  res.send(aux.rows);
 });
 
-// Add a single piece of homework, and return the added homework
-app.post("/", async (req: Request, res: Response)=>{
+/**
+ * @openapi
+ * /:
+ *     post:
+ *       description: Add homework to database. Will populate empty children's entries also. Will return the homework.
+ *       responses:
+ *          200:
+ *              description: Success - homework entry created and empty children's homework entries setup
+ */
+app.post("/", async (req: Request, res: Response) => {
+  // req.body is that right?
+  let homework = await pool.query(
+    `INSERT INTO homework (Name, Image, Datedue, Comment) VALUES ($1,$2,$3,$4) RETURNING *;`,
+    [req.body.name, req.body.image, req.body.dateDue, req.body.comment]
+  );
+  let children = await pool.query(`SELECT * FROM children;`);
 
-    // req.body is that right?
-    let homework = await pool.query(`INSERT INTO homework (Name, Image, Datedue, Comment) VALUES ($1,$2,$3,$4) RETURNING *;`,
-    [req.body.name,req.body.image, req.body.dateDue, req.body.comment]);
-    let children = await pool.query(`SELECT * FROM children;`);
+  console.log(homework);
+  console.log(children);
 
-    console.log(homework);
-    console.log(children);
+  for (let i = 0; i < children.length; i++) {
+    await pool.query(
+      `INSERT INTO childrensHomework (homeworkid, childid) VALUES ($1,$2);`,
+      [homework.id, children[i].id]
+    );
+  }
 
-    for(let i=0; i < children.length; i++)
-    {
-        await pool.query(`INSERT INTO childrensHomework (homeworkid, childid) VALUES ($1,$2);`,[homework.id, children[i].id]);
-    }
-
-
-    res.send(homework);
+  res.send(homework);
 });
 
-
-// Update a child's homework, and return the updated value
-app.put("/:homeworkId/:childId", async (req: Request, res: Response)=>{
-    let child = await pool.query(`UPDATE childrensHomework 
+/**
+ * @openapi
+ * /{homeworkId}/{childId}:
+ *     put:
+ *       description: Update a child's homework, and return the updated value
+ *       responses:
+ *          200:
+ *              description: Success - TODO
+ */
+app.put("/:homeworkId/:childId", async (req: Request, res: Response) => {
+  let child = await pool.query(
+    `UPDATE childrensHomework 
     SET image = $1, comment = $2, annotation = $3 
     WHERE homeworkid= $4 AND childid = $5 RETURNING *;`,
-    [req.body.image,req.body.comment,req.body.comment,req.params.homeworkId,req.params.childId]);
+    [
+      req.body.image,
+      req.body.comment,
+      req.body.comment,
+      req.params.homeworkId,
+      req.params.childId,
+    ]
+  );
 
-    res.send(child);
+  res.send(child);
 });
 
-app.get("/test", async (req: Request, res: Response)=>{
-    res.send([1,2,3])
+/**
+ * @openapi
+ * /{id}:
+ *     delete:
+ *       description: Delete single homework and associated childrens homework
+ *       parameters:
+ *       - in: path
+ *         description: id for the piece of homework
+ *         name: id
+ *         schema:
+ *          type: integer
+ *         required: true
+*       responses:
+ *          200:
+ *              description: Success - TODO
+ */
+app.delete("/:id", async (req: Request, res: Response) => {
+  await pool.execute(`DELETE FROM childrensHomework WHERE homeworkid=$1;`, [
+    req.params.id,
+  ]);
+
+  await pool.execute(`DELETE FROM homework WHERE id=$1;`, [req.params.id]);
 });
 
-// Delete single homework, and childrens homework
-app.delete("/:id", async (req: Request, res: Response)=>{
-    await pool.execute(`DELETE FROM childrensHomework WHERE homeworkid=$1;`,
-    [req.params.id]);
-
-    await pool.execute(`DELETE FROM homework WHERE id=$1;`,
-    [req.params.id]);
-});
-
-
-app.listen(port, ()=>{
-    console.log(`Listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
